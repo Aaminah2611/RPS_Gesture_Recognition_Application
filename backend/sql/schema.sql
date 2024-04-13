23e8f4c661aa1808cfd2a7bd5cf2913c3906bc15
Aaminah2611 <Aaminah2611@gmail.com>
2024-04-13 20:05:43 +0100

    updated app.py and game.py - now test_endpoints have no errors and web is displaying game stage/other text but no camera input yet

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,21 +1,25 @@
 CREATE TABLE User (
-    id INTEGER PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
 );
 
+INSERT INTO User (username, password) VALUES ('test_user', 'test_password');
+
+
 CREATE TABLE Game (
-    id INTEGER PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     started_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     status ENUM('pending', 'ongoing', 'finished') DEFAULT 'pending'
 );
 
 CREATE TABLE Participant (
-    id INTEGER PRIMARY KEY,
-    game_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    user_id INT NOT NULL,
     gesture VARCHAR(50),
     player_number INTEGER,
     FOREIGN KEY (game_id) REFERENCES Game(id),
     FOREIGN KEY (user_id) REFERENCES User(id)
 );
+
